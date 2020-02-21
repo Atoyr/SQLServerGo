ead37c81220ae25791230be86132bcd49c8db323
Atoyr <lessironglance@gmail.com>
2020-02-21 12:39:00 +0900

    update LineChart is apply props

--- webapps/src/components/modules/LineChart.vue
+++ webapps/src/components/modules/LineChart.vue
@@ -1,14 +1,65 @@
 <script>
-import { Line, mixins } from 'vue-chartjs'
+import { Line } from 'vue-chartjs'
 import 'chartjs-plugin-streaming';
-const { reactiveProp} = mixins
 
 export default {
   extends: Line,
-  mixins: [reactiveProp],
-  props: ['options'],
-  method() {
-    this.renderChart(this.chartData, this.options)
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  data ()  {
+    return {
+      cd: {},
+      op: null,
+    }
+  },
+  mounted () {
+    console.log(this)
+    this.cd = {datasets: [ ]}
+    //this.renderChart(this.chartdata, this.options)
+    this.renderChart(this.cd,this.op)
+    this.$data._chart.update({
+      preservation: true
+    });
+  },
+  methods: {
+    update: function () {
+      console.log(this.chartdata)
+      this.$data._chart.update({
+        preservation: true
+      });
+    },
+    addDataset: function(dataset) {
+      this.cd.datasets.push(dataset)
+    this.renderChart(this.cd,this.op)
+    },
+    applyOption: function(option) {
+      this.op = option
+    this.renderChart(this.cd,this.op)
+    },
+
+    onReceive: function (event) {
+      console.log(event)
+      this.cd.datasets[event.index].data.push({
+        x: event.timestamp,
+        y: event.value
+      });
+      this._data._chart.update({
+        preservation: true
+      });
+    }
+  },
+  plugins: {
+    streaming: {
+        frameRate: 30
+    }
   }
-}
+} 
 </script>
--- webapps/src/components/pages/dashboard.vue
+++ webapps/src/components/pages/dashboard.vue
@@ -1,22 +1,68 @@
 <template>
-  <line-chart :chart-data="readDataset"></line-chart>
-  <line-chart :chart-data="writeDataset"></line-chart>
+  <div>
+    <line-chart :chartdata="chartdata" :options="option" ref="testchart"></line-chart>
+    <button v-on:click="update()"></button>
+  </div>
 </template>
-
 <script>
+import { Line } from 'vue-chartjs'
 import LineChart from '@/components/modules/LineChart.vue';
 import 'chartjs-plugin-streaming';
+import Vue from 'vue';
 
 export default {
   name: 'dashboard',
   components: {
     LineChart
   },
-  data: () => ({
-
-  }),
+  data ()  {
+    return {
+      chartdata: {},
+      option: null,
+      time: null,
+    }
+  },
   mounted () {
-
+    this.chartdata = {
+      datasets: [{
+        label: 'Dataset 1',
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        lineTension: 0,
+        borderDash: [8, 4],
+        data: []
+      }]
+    }
+    this.option = {
+      scales: {
+        xAxes: [{
+          type: 'realtime',
+          realtime: {
+            delay: 2000,
+            refresh: 1000
+          }
+        }],
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'value'
+          }
+        }]
+      },
+      preservation: false
+    }
+    this.$refs.testchart.addDataset(this.chartdata.datasets[0])
+    this.$refs.testchart.applyOption(this.option)
+  },
+  methods : {
+    update: function() {
+      console.log(this)
+      this.$refs.testchart.onReceive({
+        index: 0,
+        timestamp: Date.now(),
+        value: 100
+      })
+    },
   }
 }
 </script>
