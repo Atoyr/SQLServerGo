4cf28cccc8d2d198c289cdca682f39e0961b0e7a
atoyr <lessironglance@gmail.com>
2020-06-15 12:08:33 +0900

    add sqlserver logo icon

--- webapps/layouts/default.vue
+++ webapps/layouts/default.vue
@@ -1,22 +1,31 @@
 <template>
   <v-app dark>
-    <v-card
-      class="toolbar-card"
-      color="grey lighten-4"
-      flat
-      tile
-      >
-      <v-toolbar dense flat class="toolbar">
-        <v-app-bar-nav-icon>
-        </v-app-bar-nav-icon>
-        <v-toolbar-title>{{dbname}}</v-toolbar-title>
-      <v-spacer></v-spacer>
-      
-      </v-toolbar>
+    <v-card >
+      <v-navigation-drawer
+        v-model="drawer"
+        :mini-variant.sync="mini"
+        permanent
+        fixed
+        app
+        temporary
+        >
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <img src="@/static/logo-sql-server.png" class="logo" @click.stop="mini = !mini"></img>
+          </v-list-item-avatar>
+          <v-list-item-title>{{dbname}} fooo</v-list-item-title>
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+        <v-divider></v-divider>
+      </v-navigation-drawer>
     </v-card>
+    <v-content>
     <v-container fluid>
       <nuxt />
     </v-container>
+    </v-content>
   </v-app>
 </template>
 
@@ -25,7 +34,22 @@ export default {
   data() {
     return {
       dbname : "",
+      drawer: true,
+      mini: true,
     }
+  },
+  fetch({ store, params}) {
+    return axios.get(`http://${this.$getHost()}/api/instance`)
+    .then((res) => {
+      console.log(res)
+    })
   }
 }
 </script>
+
+<style scoped>
+.logo {
+  width: 28px;
+  height: 28px;
+}
+</style>
--- webapps/nuxt.config.js
+++ webapps/nuxt.config.js
@@ -78,13 +78,24 @@ export default {
     }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend: (config,ctx) => {
+      const svgRule = config.module.rules.find(rule => rule.test.test('.svg'));
+
+      svgRule.test = /\.(png|jpe?g|gif|webp)$/;
+
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: [
+          'babel-loader',
+          'vue-svg-loader',
+        ],
+      });
+    },
   }
 }
